fcc4e23ebc4aea313a437b8317256e10c3896aa9
Archana Kumar <archanakumar1808@gmail.com>
2022-07-10 09:07:07 +0530

    updated footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function () {
   return (
     <div className='footer'>
-        Built by AK 2022, from &copy; "The Net Ninja"
+        Built by AK - 2022, as seen on &copy; "The Net Ninja"
     </div>
   )
 }
